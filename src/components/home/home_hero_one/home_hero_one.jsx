ca2ff2338c086957836dba664677f56cb10c7c1f
comingaround <131857839+comingaround@users.noreply.github.com>
2024-03-20 16:48:44 +0200

    commit

--- src/components/home/home_hero_one/home_hero_one.jsx
+++ src/components/home/home_hero_one/home_hero_one.jsx
@@ -1,10 +1,24 @@
-import "./home_hero_one.css"
-
+import "./home_hero_one.css";
 
 function Home_hero_one() {
-    return(
+    return (
         <div className="home_hero_one">
+            <div className="moving_text_wrapper">
+                <div className="moving_text">
+                    <span>Green and convenient.  Green and convenient.</span>
+                    <span>Green and convenient.  Green and convenient.</span>
+                </div>
+            </div>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-6">
 
+                    </div>
+                    <div className="col-lg-6">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
